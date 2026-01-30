5bee01504bce812994447ebc427f16aa388ae018
Manya Gupta <e1430708@u.nus.edu>
2026-01-30 20:57:08 +0800

    Level-2: Add, List

--- src/main/java/Mercury.java
+++ src/main/java/Mercury.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Mercury {
@@ -7,9 +8,18 @@ public class Mercury {
         System.out.println("_________________________________________________");
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         String userinput = myObj.nextLine();  // Read user input
+        ArrayList userlist = new ArrayList();
         while (!userinput.equals("bye")) {
             System.out.println("_________________________________________________");
-            System.out.println(userinput);
+            if (userinput.equals("list")) {
+                System.out.println(userinput);
+                for (int i = 0; i < userlist.size(); i++) {
+                    System.out.println(i + ". " + userlist.get(i));
+                }
+            }
+            else {
+                System.out.println("added: " + userinput);
+                userlist.add(userinput);}
             System.out.println("_________________________________________________");
             userinput = myObj.nextLine();
         }

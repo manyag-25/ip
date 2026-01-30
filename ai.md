8243daee5b9ca151f3c0c0fa379c73ee5d065214
Manya Gupta <e1430708@u.nus.edu>
2026-01-30 21:10:53 +0800

    Level-3: Mark as Done

---
+++ ai.md
@@ -0,0 +1,2 @@
+Week 3:
+Level-3 - used claude to make my change of the arraylist to ArrayList<String[]> consistent across all parameters
--- src/main/java/Mercury.java
+++ src/main/java/Mercury.java
@@ -8,22 +8,46 @@ public class Mercury {
         System.out.println("_________________________________________________");
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         String userinput = myObj.nextLine();  // Read user input
-        ArrayList userlist = new ArrayList();
+        ArrayList<String[]> userlist = new ArrayList<>();
+
         while (!userinput.equals("bye")) {
             System.out.println("_________________________________________________");
+
             if (userinput.equals("list")) {
                 System.out.println(userinput);
                 for (int i = 0; i < userlist.size(); i++) {
-                    System.out.println(i + ". " + userlist.get(i));
+                    String[] task = userlist.get(i);
+                    System.out.println((i + 1) + ". [" + task[1] + "] " + task[0]);
+                }
+            }
+            else if (userinput.startsWith("mark")) {
+                int taskIndex = Integer.parseInt(userinput.substring(5)) - 1;
+                if (taskIndex >= 0 && taskIndex < userlist.size()) {
+                    userlist.get(taskIndex)[1] = "X";
+                    System.out.println("I've marked this task as done:\n" +
+                            "[X] " + userlist.get(taskIndex)[0]);
+                }
+            }
+            else if (userinput.startsWith("unmark")) {
+                int taskIndex = Integer.parseInt(userinput.substring(7)) - 1;
+                if (taskIndex >= 0 && taskIndex < userlist.size()) {
+                    userlist.get(taskIndex)[1] = " ";
+                    System.out.println("OK, I've marked this task as not done yet:\n" +
+                            "[ ] " + userlist.get(taskIndex)[0]);
                 }
             }
             else {
+                String[] newTask = {userinput, " "};
+                userlist.add(newTask);
                 System.out.println("added: " + userinput);
-                userlist.add(userinput);}
+            }
+
             System.out.println("_________________________________________________");
             userinput = myObj.nextLine();
         }
+
         System.out.println("Bye. Hope to see you again soon!");
+        myObj.close();
         System.exit(0);
     }
 }

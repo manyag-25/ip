a26477a1abb7a51ecc22fcabf46cc9211173d6cf
Manya Gupta <e1430708@u.nus.edu>
2026-01-30 20:50:31 +0800

    Revert "Level-2: Add, List"
    This reverts commit 0031534e253a8fc140a34eece0820e28772e4cf6.

--- src/main/java/Mercury.java
+++ src/main/java/Mercury.java
@@ -1,19 +1,8 @@
-import java.util.Scanner;
-
 public class Mercury {
     public static void main(String[] args) {
         String name = "Mercury";
         System.out.println("Hello! I'm " + name + "\n" + "What can I do for you?");
         System.out.println("_________________________________________________");
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        String userinput = myObj.nextLine();  // Read user input
-        while (!userinput.equals("bye")) {
-            System.out.println("_________________________________________________");
-            System.out.println(userinput);
-            System.out.println("_________________________________________________");
-            userinput = myObj.nextLine();
-        }
         System.out.println("Bye. Hope to see you again soon!");
-        System.exit(0);
     }
 }
